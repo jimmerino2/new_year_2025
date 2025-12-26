95b7d4ee1700715d67c5ce2e38207cde6935fd34
Jim <141894353+jimmerino2@users.noreply.github.com>
2025-12-26 21:06:00 +0800

    feat: video compressor to fit vercel conditions

---
+++ public/core/PageHandler.js
@@ -0,0 +1,50 @@
+function setCurrentPage(pageNo = undefined) {
+  if (pageNo !== undefined) {
+    localStorage.setItem("page", pageNo);
+  }
+
+  const savedPage = localStorage.getItem("page");
+  if (savedPage === null) {
+    localStorage.setItem("page", "0");
+  }
+
+  goToCurrentPage();
+}
+
+function updatePage(type) {
+  let currentPage = Number(localStorage.getItem("page")) || 0;
+  if (type === "back") {
+    currentPage = Math.max(0, currentPage - 1);
+  } else if (type === "next") {
+    currentPage = currentPage + 1;
+  }
+  localStorage.setItem("page", currentPage);
+  goToCurrentPage();
+}
+
+function goToCurrentPage() {
+  const page = localStorage.getItem("page") || "0";
+  const pageMap = {
+    0: "00_intro.html",
+    1: "01_jan.html",
+    2: "02_feb.html",
+    3: "03_apr.html", // Skip March
+    4: "04_may.html",
+    5: "05_misc.html",
+    6: "06_dec.html",
+    7: "07_outro.html",
+  };
+
+  const targetFile = pageMap[page];
+  const currentPath = window.location.pathname;
+
+  if (currentPath.endsWith(targetFile)) return;
+
+  const isInsidePages = currentPath.includes("/pages/");
+
+  if (isInsidePages) {
+    window.location.href = targetFile;
+  } else {
+    window.location.href = `pages/${targetFile}`;
+  }
+}
